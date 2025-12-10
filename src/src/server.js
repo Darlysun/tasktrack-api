e9748714b1287d692c5e536467e3458e7d7df948
Darlysun <mendoncadarlysson34@gmail.com>
2025-12-09 21:19:18 -0300

    Add server.js file

---
+++ src/src/server.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.use("/auth", require("./routes/authRoutes"));
+app.use("/tasks", require("./routes/taskRoutes"));
+
+app.listen(3000, () => console.log("Server running on port 3000"));
